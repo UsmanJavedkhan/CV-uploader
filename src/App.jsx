4353fdc3e61e503ec715f65e9ee73a969f9f6e3c
UsmanJavedkhan <usmanjk029@gmail.com>
2025-09-11 20:07:45 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import CVUploader from './components/CVUploader.jsx'
+
+function App() {
+ 
+
+  return (
+    <>
+  
+<CVUploader />
+
+
+
+    </>
+  )
+}
+
+export default App
